54dd083c8a306fb92ce03ba304642ef60d1ce7bc
hondahmd <mingda.hong@qq.com>
2019-09-17 17:40:22 +0800

    little optimization

--- week-01/day-02/13.js
+++ week-01/day-02/13.js
@@ -1,12 +1,7 @@
 'use strict'
 
-function greet(input) {
-    if (input === undefined) {
-        console.log("no input, printing default");
-        console.log("Greetings, al");
-    } else {
-        console.log(`Greetings, dear ${input}`);
-    }
+function greet(input = "al") {
+    console.log(`Greetings, dear ${input}`);
 }
 
 let al = 'EPAM';
