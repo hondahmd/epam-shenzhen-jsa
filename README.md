0f70cebee5b04828b2554844cdd757e7c9ee94ff
hondahmd <mingda.hong@qq.com>
2019-09-17 16:34:28 +0800

    littel optimization

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-good good study, day day up.
+1. Good good study, day day up.
+2. Simple is beautiful.
--- week-01/day-02/08.js
+++ week-01/day-02/08.js
@@ -1,33 +1,29 @@
 'use strict';
 
+function drawLine(i, half) {
+    let line = '';
+    for (let j = 0; j < 2 * half - 1; j++) {
+        if (j < half - i - 1 || j > half + i - 1) {
+            line += ' ';
+        } else {
+            line += '*';
+        }
+    }
+    return line;
+}
+
 const lineCount = 7;
 let half = Math.round(lineCount / 2);
+let result = new Array(lineCount);
 
 for (let i = 0; i < half; i++) {
-    let line = '';
-    for (let j = 0; j < 2 * half - 1; j++) {
-        if (j < half - i - 1 || j > half + i - 1) {
-            line += ' ';
-        } else {
-            line += '*';
-        }
-    }
-    console.log(line);
+    result[i] = drawLine(i, half);
 }
-
-half = lineCount - half;
 
 for (let i = half - 1; i >= 0; i--) {
-    let line = '';
-    if (lineCount % 2 !== 0) {
-        line += ' ';
-    }
-    for (let j = 0; j < 2 * half - 1; j++) {
-        if (j < half - i - 1 || j > half + i - 1) {
-            line += ' ';
-        } else {
-            line += '*';
-        }
-    }
-    console.log(line);
+    result[lineCount - i - 1] = drawLine(i, half);
 }
+
+result.forEach(function(item) {
+    console.log(item);
+});
