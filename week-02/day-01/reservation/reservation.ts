99d0768259312348d5f72729fc62fd41c348781d
hondahmd <mingda.hong@qq.com>
2019-09-23 18:00:16 +0800

    remove duplicate dirs

--- week-02/day-01/reservation/reservation.ts
+++ week-02/day-01/reservation/reservation.ts
@@ -3,17 +3,21 @@ import Reservationy from "./reservationy";
 class Reservation implements Reservationy {
     private numberOfReservations: number;
     private codes: string;
+    private codeLength: number;
     private days: string[];
+    private bookingNumber: number;
 
-    constructor(numberOfReservations: number) {
+    constructor(numberOfReservations: number, codeLength: number = 8, bookingNumber: number = 10) {
         this.numberOfReservations = numberOfReservations;
+        this.codeLength = codeLength;
         this.codes = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         this.days = ["MON", "TUE", "WES", "THU", "FRI", "SAT", "SUN"];
+        this.bookingNumber = bookingNumber;
     }
 
     public getCodeBooking(): string {
         let code = "";
-        for (let i = 0; i < 8; i++) {
+        for (let i = 0; i < this.codeLength; i++) {
             code += this.codes.charAt(Math.floor(Math.random() * this.codes.length));
         }
         return code;
@@ -24,7 +28,7 @@ class Reservation implements Reservationy {
     }
 
     public generateBooking(): void {
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < this.bookingNumber; i++) {
             console.log(`Booking# ${this.getCodeBooking()} for ${this.getDowBooking()}`);
         }
     }
